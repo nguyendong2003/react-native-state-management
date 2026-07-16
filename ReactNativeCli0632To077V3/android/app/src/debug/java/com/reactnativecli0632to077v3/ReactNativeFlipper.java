5b586214f8a35ac476c22df22929fe76adc7aeb7
rs-dongnt1 <dongnt1@runsystem.net>
2026-07-16 14:32:01 +0700

    init ReactNativeCli0632To077V3 (has error)

---
+++ ReactNativeCli0632To077V3/android/app/src/debug/java/com/reactnativecli0632to077v3/ReactNativeFlipper.java
@@ -0,0 +1,10 @@
+package com.reactnativecli0632to077v3;
+
+import android.content.Context;
+import com.facebook.react.ReactInstanceManager;
+
+public class ReactNativeFlipper {
+  public static void initializeFlipper(Context context, ReactInstanceManager reactInstanceManager) {
+    // Flipper is disabled.
+  }
+}
